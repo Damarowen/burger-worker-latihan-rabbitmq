0f3f2aebf97f9887fce75e76b2606bb7be3314a0
fdn-damaroenisyaai@owen-fdn <damar@femaledaily.com>
2022-06-29 21:55:03 +0700

    set up

--- producers/libs/rabbitmq/src/rabbitmq.service.ts
+++ producers/libs/rabbitmq/src/rabbitmq.service.ts
@@ -9,12 +9,12 @@ export class RabbitMQService {
 
 
     public async send(route: string, message: any) {
-       
+
         try {
             console.log("🚀 ~ file: rabbitmq.service.ts ~ line 12 ~ RabbitMQService ~ send ~ message", message)
             console.log("🚀 ~ file: rabbitmq.service.ts ~ line 12 ~ RabbitMQService ~ send ~ route", route)
             Logger.log(`MESSAGE IS BEING SEND 😋`);
-            await this.amqpCon.publish("", route, message)
+            this.amqpCon.publish("", route, message)
         } catch (error) {
             console.log(error)
         }
--- producers/src/app.controller.ts
+++ producers/src/app.controller.ts
@@ -14,11 +14,47 @@ export class AppController {
     @Body() createPost,
   ) {
     try {
+      const route = "send_email_winner_new"
+      const data = {
+        user: {
+          id: "damar",
+          username: "damarowen",
+          email: "damarowen@gmail.com"
+        },
+        "email": {
+          "subject": "DARI FDBR newww",
+          "body": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        }
+      }
+      await this.rabbitMQService.send(route, data); // 1
+      await this.rabbitMQService.send(route, data); // 2
+      await this.rabbitMQService.send(route, data); // 3
+      await this.rabbitMQService.send(route, data);// 4
+      await this.rabbitMQService.send(route, data); // 5
+      await this.rabbitMQService.send(route, data); //6
+      await this.rabbitMQService.send(route, data);// 7
+      await this.rabbitMQService.send(route, data);// 8
+      await this.rabbitMQService.send(route, data);// 9
+      await this.rabbitMQService.send(route, data);// 10
+      await this.rabbitMQService.send(route, data);// 11
+      await this.rabbitMQService.send(route, data);// 12
+      await this.rabbitMQService.send(route, data);// 13
+      await this.rabbitMQService.send(route, data);// 14
+      await this.rabbitMQService.send(route, data);// 15
 
-      const { route, data } = createPost
 
-      await this.rabbitMQService.send(route,data);
-      return { route, data }
+      const wait = (timeToDelay: number): any =>
+        new Promise(resolve => setTimeout(resolve, timeToDelay))
+
+
+      await wait(10000)
+      // return { route, data }
+      const resp = {
+        "message": "ok"
+      }
+
+      return resp
+
     } catch (error) {
       console.log(error)
     }
